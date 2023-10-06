b4a18ce8f1573febbce8532428853cc864e11a7a
Praneet Kumar Singh <114861239+PraneetSingh242@users.noreply.github.com>
2023-10-06 16:26:08 +0000

    dataTypes & typeof

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //It treats all JS code as newer version
+
+// alert(3+3) // We are using node.js, not browser so it will throw an error.
+
+console.log(3+3) //Code readability should be high
+console.log("Hitesh")
+
+// alert("Hello")
+
+let name ="Hitesh" //string
+let age = 21 //number
+let isLoggedIn = false
+let state; //undefined
+
+//number => 2**53
+//bigint
+//string => "",''
+// Boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => Uniqueness
+
+// Object
+
+console.log(typeof "");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
